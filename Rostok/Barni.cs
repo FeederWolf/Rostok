c7b36e28543724ba013196f4bc4909604f11ed1b
FeederWolf <91455735+FeederWolf@users.noreply.github.com>
2022-03-29 10:49:00 +0200

    Add project files.

---
+++ Rostok/Barni.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rostok
+{
+    class Barni
+    {
+        public string megnevezes { get; set; }
+        public string kategoria { get; set; }
+        public string egyseg { get; set; }
+        public double rost { get; set; }
+
+        public Barni(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            megnevezes = s[0];
+            kategoria = s[1];
+            egyseg = s[2];
+            rost = double.Parse(s[3]);
+        }
+    }
+}
---
+++ Rostok/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Rostok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Barni> lista = new List<Barni>();
+            foreach (var i in File.ReadAllLines("rostok.txt").Skip(1))
+            {
+                lista.Add(new Barni(i));
+            }
+            
+        }
+    }
+}
